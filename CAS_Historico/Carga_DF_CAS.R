fb64279e06be9815cffd4a105e868dfdb43c6929
Jobcito <jorge.orellana.cl@outlook.com>
2025-07-31 14:00:51 -0400

    Pull 31-07-2025-14-00

--- CAS_Historico/Carga_DF_CAS.R
+++ CAS_Historico/Carga_DF_CAS.R
@@ -3,7 +3,8 @@
 # Se utiliza data.table para una carga más eficiente de grandes datasets.
 
 library(data.table)
-
+# El archivo CSV venia en codificación UTF-16 por lo que se convirtió a UTF-8 con el siguiente comando:
+# iconv -f UTF-16 -t UTF-8 004-Carga_Académica_Semestral_REP006_historico.csv -o 004-Carga_Académica_Semestral_REP006_historico_utf8.csv 
 datos <- fread("004-Carga_Académica_Semestral_REP006_historico_utf8.csv")
 
 # Mostrar las primeras filas del dataframe, su estructura y un resumen estadístico
