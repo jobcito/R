e6fd676bec9fc300fd88a305816106334c9637a9
Jobcito <jorge.orellana.cl@outlook.com>
2025-07-31 13:46:50 -0400

    Sync  por nueva carpeta "CAS" 31-07-2025-13-46

---
+++ CAS_Historico/Carga_DF_CAS.R
@@ -0,0 +1,19 @@
+# Cargar las librerías necesarias, se hace de esta forma por el tamaño del archivo
+# y para evitar problemas de memoria al cargar el dataframe completo.
+# Se utiliza data.table para una carga más eficiente de grandes datasets.
+
+library(data.table)
+
+datos <- fread("004-Carga_Académica_Semestral_REP006_historico_utf8.csv")
+
+# Mostrar las primeras filas del dataframe, su estructura y un resumen estadístico
+head(datos)
+str(datos)
+summary(datos)
+
+# Filtrar por RUT, sin filtrar por columna especifica
+registros_filtrados <- datos[datos$RUT == "19637245", ]
+
+# Mostrar los registros filtrados en un grid
+View(registros_filtrados)
+
